2358df8e4e6743d75e07ffabe3b534ab98706ac0
kavvi-sri <84309664+kavvi-sri@users.noreply.github.com>
2021-07-10 13:39:14 +0530

    Add files via upload

---
+++ Encapsulation/Encapsulation/Main/Program.cs
@@ -0,0 +1,35 @@
+﻿using Encapsulation.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        public static void displayStudent()
+        {
+
+            Student std = new Student();
+            std.Id = 101;
+            std.Name = "kavya";
+            std.Email = "sri@gmail.com";
+            std.Mobilenumber = 9876654321;
+            std.Course = "cse";
+            std.Pincode = 505324;
+            Console.WriteLine ("\nId:" + std.Id + "\nName:" + std.Name + "\nEmail:" + std.Email + "\nMobilenumber:" + std.Mobilenumber + "\nCourse:" + std.Course +
+                "\nPincode:" + std.Pincode);
+            Console.ReadLine();
+
+        }
+        static void Main(string[] args)
+        {
+            Program.displayStudent();
+
+        }
+
+       
+    }
+}
---
+++ Encapsulation/Encapsulation/Model/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation.Model
+{
+    class Student
+    {
+        private int id;
+        private string name;
+        private string email;
+        private long mobilenumber;
+        private string course;
+        private int pincode;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string  Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public long Mobilenumber
+        {
+            get { return mobilenumber; }
+            set { mobilenumber = value; }
+
+        }
+        public string Course
+        {
+            get { return course; }
+            set { course = value; }
+        }
+        public int Pincode
+        {
+            get { return pincode; }
+            set { pincode = value; }
+        }
+
+       
+
+
+
+    }
+}
