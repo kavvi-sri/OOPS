b95bbd74eed23faccecbde8fcc7c1529c38fdff6
kavvi-sri <84309664+kavvi-sri@users.noreply.github.com>
2021-07-11 21:43:13 +0530

    Add files via upload

---
+++ List Example/List Example/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> age = new List<int>();
+            age.Add(10);
+            age.Add(20);
+            age.Add(30);
+            age.Add(40);
+            age.Add(50);
+
+            foreach(int i in age)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<string> colors = new List<string>();
+            colors.Add("Pink");
+            colors.Add("Black");
+            colors.Add("Red");
+            colors.Add("Green");
+            colors.Add("Red");
+
+            // colors.Remove("Pink");
+
+            //To Remove Duplicates
+            HashSet<string> hashcolors = new HashSet<string>(colors);
+            
+
+            foreach (string color in hashcolors) 
+            {
+                Console.WriteLine(color);
+            }
+
+
+           /* string s = "kavya";
+
+            int sLength = s.Length;
+
+            Console.WriteLine(s+ +sLength);
+
+            Console.WriteLine(s.ToUpper());*/
+
+
+
+
+        }
+    }
+}
